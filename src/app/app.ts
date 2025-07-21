91e821f91a9b9a1e5ddab4fc06c90862d02aa96e
Arvind Kumar <arvind2k20@gmail.com>
2025-07-21 14:45:37 +0530

    Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'myangular-app';
+ demo = 'abc';
+
+
+students = ['Ramesh', 'Suresh'];
+
+
+constructor() {
+
+this.demo = 'new Value'
+
+}
+
+
+greetme()
+{
+var title = 'Arvind'
+console.log(title);
+}
+
 }
